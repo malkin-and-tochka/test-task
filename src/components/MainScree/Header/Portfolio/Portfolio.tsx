c99f1f82e3412e51dd6cb421624708933b3db525
ogyre <ogyrec2019@gmail.com>
2023-08-12 12:22:08 +0300

    add: portfolio list button Previously, you had to click on the cell

--- src/components/MainScree/Header/Portfolio/Portfolio.tsx
+++ src/components/MainScree/Header/Portfolio/Portfolio.tsx
@@ -1,24 +1,24 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import style from './Portfolio.module.scss'
 import {PortfolioContext} from "../../../../Context/PortfolioContext";
 import {TempPortfolioPrice} from "../../../../Context/TempPortfolioPrice";
 
 type PortfolioType = {
-    setPortfolioModalWindow: (bool: boolean)=>void
+    setPortfolioModalWindow: (bool: boolean) => void
 }
 
 const Portfolio: React.FC<PortfolioType> = ({setPortfolioModalWindow}) => {
     const tempPriceContext = useContext(TempPortfolioPrice)
-    console.log(tempPriceContext.sessionPrice)
     return (
         <PortfolioContext.Consumer>
             {theme =>
-                <div onClick={()=>setPortfolioModalWindow(true)} className={style.container}>
+                <div  className={style.container}>
                     Portfolio:
                     <div>
                         <span>{parseFloat(String(theme.initialState.totalCost - tempPriceContext.sessionPrice)).toFixed(2)} USD</span><br/>
                         <span>+ {theme.initialState.totalCost ? parseFloat(String(tempPriceContext.sessionPrice)).toFixed(2) : 0} USD{tempPriceContext.sessionPrice ? <> ({parseFloat(String((tempPriceContext.sessionPrice / theme.initialState.totalCost) * 100)).toFixed(2)}%)</> : null}</span>
                     </div>
+                    <button className={style.listButton} onClick={() => setPortfolioModalWindow(true)}>List</button>
                 </div>}
         </PortfolioContext.Consumer>
     );
